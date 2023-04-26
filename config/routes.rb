14809e322664f6f17315df0ddb9f6d1db73fa144
robustcicada <suda@saice.co.jp>
2023-04-26 15:04:06 +0900

    ログイン機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :tasks
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
